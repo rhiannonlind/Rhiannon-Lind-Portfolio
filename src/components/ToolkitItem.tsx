48987b675e016c0a29e8df8468217cf84f908c26
rhiannonlind <133162388+rhiannonlind@users.noreply.github.com>
2025-06-08 12:55:36 +0100

    Responsive: Fix toolkit item width and font size for mobile grid

--- src/components/ToolkitItem.tsx
+++ src/components/ToolkitItem.tsx
@@ -12,10 +12,10 @@ const ToolkitItem: React.FC<ToolkitItemProps> = ({ title, imageSrc, imageAlt, it
   return (
     <div className={`bg-white rounded-lg w-[300px] h-auto flex flex-col pb-4 ${itemClass}`}>
       <div className="h-[60px] flex items-center justify-center">
-        <h4 className="text-center heading-4">{title}</h4>
+        <h4 className="text-center heading-4 toolkit-heading-4">{title}</h4>
       </div>
       <div className="flex-1 flex items-start justify-center px-4 py-2">
-        <div className={`w-[120px] h-[120px] flex items-center justify-center ${imgClass}`}>
+        <div className={`toolkit-img w-[120px] h-[120px] flex items-center justify-center ${imgClass}`}>
           <img 
             src={imageSrc} 
             alt={imageAlt} 
